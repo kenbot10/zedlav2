d19fdd7590447be10aaf52d02c6296169fcd3eed
kenbot10 <kbuddy19@icloud.com>
2025-09-15 08:41:47 -0400

    agro script

--- v2/Assets/script/agro.cs
+++ v2/Assets/script/agro.cs
@@ -8,6 +8,7 @@ public class agro : MonoBehaviour
     public Transform player;
 
     public LayerMask whatIsGround, whatIsPlayer;
+    public float health;
 
     //patroling
     public Vector3 walkPoint;
@@ -17,6 +18,7 @@ public class agro : MonoBehaviour
     //attacking
     public float timeBetweenAttacks;
     bool alreadyAttacked;
+    public GameObject projectile;
 
     //states
     public float sightRange, attackRange;
@@ -68,9 +70,46 @@ public class agro : MonoBehaviour
     }
     private void AttackPlayer()
     {
-        //make sure enemy doesm't mave
+        //make sure enemy doesn't move
         agent.SetDestination(transform.position);
 
         transform.LookAt(player);
+
+        if (!alreadyAttacked)
+        {
+            ///Attack code here 
+            Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
+
+            rb.AddForce(transform.forward * 32f, ForceMode.Impulse);
+            rb.AddForce(transform.up * 8f, ForceMode.Impulse);
+
+            ///
+
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+    }
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        if (health <= 0) Invoke(nameof(DestoryEnemy), .5f);
+    }
+    private void DestoryEnemy()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
     }
 }

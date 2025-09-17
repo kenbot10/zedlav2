00e7fac9ec51f4c6003c4e363321e037ac6a3385
kenbot10 <kbuddy19@icloud.com>
2025-09-17 08:39:28 -0400

    health bar 50%

--- v2/Assets/Scripts/Playerstats.cs
+++ v2/Assets/Scripts/Playerstats.cs
@@ -5,8 +5,26 @@ public class Playerstats : MonoBehaviour
     [SerializeField] private float maxhealth;
 
     private float currenthealth;
+
+    public HealthBar healthBar;
     private void Start()
     {
         currenthealth = maxhealth;
+
+        healthBar.SetSliderMax(maxhealth);
+    }
+
+    public void TakeDamage(float amount)
+    {
+        currenthealth -= amount;
+        healthBar.SetSlider(currenthealth);
+    }
+    private void Update()
+    {
+        ///if the player presses the "k" key
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            TakeDamage(20f);
+        }
     }
 }

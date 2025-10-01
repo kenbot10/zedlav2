2a316ce3a5ebf6f917dbd9bbd6957ccd757918d4
kenbot10 <kbuddy19@icloud.com>
2025-10-01 10:10:20 -0400

    working guns!

---
+++ v2/Assets/Scripts/gunScript.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class gunScript : MonoBehaviour
+{
+    [SerializeField] private LayerMask RandomCubesLayer;
+    [SerializeField] private float damage;
+
+    private Camera beancam;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        beancam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray gunray = new Ray(beancam.transform.position, beancam.transform.forward);
+
+            if (Physics.Raycast(gunray, out RaycastHit hitInfo, 1000f, RandomCubesLayer))
+            { 
+               if(hitInfo.collider.gameObject.TryGetComponent(out agro CubeHit))
+                {
+                    CubeHit.TakeDamage ((int)damage);
+                    Debug.Log(CubeHit.health);
+                }
+            }
+        }
+    }
+}
